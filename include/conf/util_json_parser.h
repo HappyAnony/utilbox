d2eac39ceff7f6d32066eca8c2fd28934e318344
HappyAnony <13545108786@163.com>
2020-05-06 21:18:08 +0800

    Add json-parser

---
+++ include/conf/util_json_parser.h
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2020-2030, HappyAnony <13545108786@163.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Redis nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS AS IS
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __UTIL_JSON_PARSER_H__
+#define __UTIL_JSON_PARSER_H__
+
+#include <conf/util_json.h>
+
+
+/*
+ * Simple json-parser implementation based on json-c library
+ *
+ * Json-parser parses and encapsulates json data as JSON-OID to JSON-OBJECT
+ *
+ * JSON-OBJECT: struct json_object object in json-c library
+ *
+ * JSON-OID: similar to SNMP-OID
+ * For example:
+ * {
+ *     "a" : {
+ *        "b" : [
+ *           {
+ *              "c" : [d,e]
+ *           },
+ *           {
+ *              "f" : [[g]]
+ *           }
+ *        ]
+ *     }
+ * }
+ * the JSON-OID corresponding to c is a.b[0].c
+ * the JSON-OID corresponding to e is a.b[0].c[1]
+ * the JSON-OID corresponding to g is a.b[1].f[0][0]
+ *
+ *
+ * JSON-OID is consists of JSON-FIELD
+ * For example: a.b[1].f[0][0]
+ *      a.b[1] is the first JSON-FIELD
+ *      f[0][0] is the second JSON-FIELD
+ *
+ *
+ * JSON-FIELD is consists of JSON-DICT and JSON_ARRAY
+ * For example: a.b[1]
+ *      a.b is JSON-DICT
+ *      [1] is JSON-ARRAY
+ *
+ * There is a length limit on JSON-DICT, the length of a.b is three
+ * There is a depth limit on JSON-ARRAY, the depth of [0][0] is two
+ * */
+
+struct json_parser_attr {
+    /* The maximum number of fields in JSON-OID supported by the json-parser
+     * defalut value is defined by JSON_FIELD_NUM_MAX macro*/
+    unsigned int json_field_num;
+
+    /* The maximum length of JSON-DICT in JSON-FIELD supported by the json-parser
+     * defalut value is defined by JSON_DICT_LEN_MAX macro*/
+    unsigned int json_dict_length;
+
+    /* The maximum depth of JSON-ARRAY in JSON-FIELD supported by the json-parser
+     * defalut value is defined by JSON_ARRAY_DEPTH_MAX macro*/
+    unsigned int json_array_depth;
+};
+
+struct json_array {
+    bool         exist;
+    unsigned int index;
+};
+
+struct json_field {
+    bool               exist;
+    char               *json_dict;
+    struct json_array  *json_arry;
+};
+
+struct json_parser {
+    /* the attribution of json-parser*/
+    struct json_parser_attr   attr;
+
+    /* parse the JSON-OID and save the JSON-FIELD */
+    struct json_field         *json_field;
+
+    /* point to the JSON-OBJECT generated from parsing json-data or creating */
+    struct json_object        *json_object;
+};
+
+
+typedef enum {
+    /* create a struct json_object JSON-OBJECT */
+    JSON_OP_NEW,
+    JSON_OP_NEW_DICT,   /* create {} JSON-OBJECT */
+    JSON_OP_NEW_ARRAY,  /* create [] JSON-OBJECT */
+
+
+    /* parse the json-data(json-string or json-file and so on) and generate the struct json_object JSON-OBJECT */
+    JSON_OP_PARSE,
+
+
+    /* add field to JSON-OBJECT by JSON-OID */
+    JSON_OP_ADD,
+
+
+    /* get the value of field in JSON-OBJECT by JSON-OID */
+    JSON_OP_GET,
+
+
+    /* update the value of field in JSON-OBJECT by JSON-OID */
+    JSON_OP_UPDATE,
+
+
+    /* delete the value of field in JSON-OBJECT by JSON-OID */
+    JSON_OP_DELETE,
+
+
+    /* dump the struct json_object JSON-OBJECT to json-data, such as json-string or json-file */
+    JSON_OP_DUMP,
+
+    JSON_OP_NUM
+} json_op;
+
+struct json_op_attr{
+    json_op         op_type;
+    union {
+
+    }               op_data;
+};
+
+struct json_parser* json_parser_create(struct json_parser_attr *attr);
+int json_parser_ctl(struct json_parser* json_parser, json_op op, struct json_op_attr *op_attr);
+int json_parser_free(struct json_parser* json_parser);
+
+#endif /* __UTIL_JSON_PARSER_H__ */
---
+++ source/conf/util_json_parser.c
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2020-2030, HappyAnony <13545108786@163.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Redis nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS AS IS
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <conf/util_json_parser.h>
+
+
+#define   JSON_FIELD_NUM_MAX       (4)
+#define   JSON_DICT_LEN_MAX        (200)
+#define   JSON_ARRAY_DEPTH_MAX     (2)
+
+struct json_parser* json_parser_create(struct json_parser_attr *attr)
+{
+    struct json_parser *json_parser = NULL;
+
+    do {
+        json_parser = calloc(1, sizeof(struct json_parser));
+        if (!json_parser) break;
+
+        /* attribution assignment */
+        json_parser->attr.json_field_num    = ((attr && attr->json_field_num  ) ? attr->json_field_num   : JSON_FIELD_NUM_MAX  );
+        json_parser->attr.json_dict_length  = ((attr && attr->json_dict_length) ? attr->json_dict_length : JSON_DICT_LEN_MAX   );
+        json_parser->attr.json_array_depth  = ((attr && attr->json_array_depth) ? attr->json_array_depth : JSON_ARRAY_DEPTH_MAX);
+
+        /* alloc memory for JSON-FIELD */
+        struct json_field *json_field_memory = calloc(json_parser->attr.json_field_num,
+                sizeof(struct json_field));
+        char              *json_dict_memory  = calloc(json_parser->attr.json_field_num * json_parser->attr.json_dict_length,
+                sizeof(char));
+        struct json_array *json_array_memory = calloc(json_parser->attr.json_field_num * json_parser->attr.json_array_depth,
+                sizeof(struct json_array));
+        if (!json_field_memory || !json_dict_memory || !json_array_memory)
+        {
+            if (json_field_memory) free(json_field_memory);
+            if (json_dict_memory)  free(json_dict_memory);
+            if (json_array_memory) free(json_array_memory);
+            free(json_parser);
+            json_parser = NULL;
+            break;
+        }
+
+        json_parser->json_field = json_field_memory;
+        unsigned int field = 0;
+        for (field = 0; field < json_parser->attr.json_field_num; field++)
+        {
+            json_parser->json_field[field].json_dict = json_dict_memory +
+                    (field * (json_parser->attr.json_dict_length) * (sizeof(char)));
+            json_parser->json_field[field].json_arry = json_array_memory +
+                    (field * (json_parser->attr.json_array_depth) * (sizeof(struct json_array)));
+        }
+    } while(0);
+
+    return json_parser;
+}
+int json_parser_ctl(struct json_parser* json_parser, json_op op, struct json_op_attr *op_attr)
+{
+    return 0;
+}
+int json_parser_free(struct json_parser* json_parser)
+{
+    return 0;
+}
